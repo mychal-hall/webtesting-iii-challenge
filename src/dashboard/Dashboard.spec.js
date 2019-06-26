fc7b6bc25670c49788b85b8ba5573bea8b07916a
Mychal <hallmychal@gmail.com>
2019-06-25 22:11:05 -0500

    Dashboard Tested

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,26 @@
-// Test away
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+import renderer from "react-test-renderer";
+
+import Dashboard from "./Dashboard";
+
+afterEach(cleanup);
+
+describe("<Dashboard />", () => {
+  it("Matches snap", () => {
+    const { container } = render(<Dashboard />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("Renders", () => {
+    render(<Dashboard />);
+  });
+
+  it("Renders all children", () => {
+    render(<Dashboard />);
+
+    expect(document.querySelector(".controls")).toBeTruthy();
+    expect(document.querySelector(".display")).toBeTruthy();
+  });
+});
